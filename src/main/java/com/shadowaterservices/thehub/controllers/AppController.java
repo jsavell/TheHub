3fceaa489a8a240376748808625768a8c5ba6096
jsavell <contact@shadowater.com>
2016-02-27 20:48:42 -0600

    code cleanup

--- src/main/java/com/shadowaterservices/thehub/controllers/AppController.java
+++ src/main/java/com/shadowaterservices/thehub/controllers/AppController.java
@@ -1,7 +1,6 @@
 package com.shadowaterservices.thehub.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,14 +12,6 @@ public class AppController {
 	@Autowired
 	UserRepository userRepository;
 
-	/*
-    @RequestMapping("/")
-    public String index(Model model) {
-        model.addAttribute("users", userRepository.findAll());
-        return "index";
-    }
-    */
-    
     @RequestMapping("/")
     public ModelAndView index() {
         ModelAndView mav = new ModelAndView("users");
