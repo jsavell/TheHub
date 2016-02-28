07d4b84121b60b2d37d06bdc44525806e1544954
jsavell <contact@shadowater.com>
2016-02-27 20:48:03 -0600

    working thymeleaf resolving

--- src/main/java/com/shadowaterservices/thehub/controllers/AppController.java
+++ src/main/java/com/shadowaterservices/thehub/controllers/AppController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.shadowaterservices.thehub.data.repositories.UserRepository;
 
@@ -11,11 +12,20 @@ import com.shadowaterservices.thehub.data.repositories.UserRepository;
 public class AppController {
 	@Autowired
 	UserRepository userRepository;
-	
+
+	/*
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "index";
+    }
+    */
+    
+    @RequestMapping("/")
+    public ModelAndView index() {
+        ModelAndView mav = new ModelAndView("users");
+        mav.addObject("users", userRepository.findAll());
+        return mav;
     }
 
 }
