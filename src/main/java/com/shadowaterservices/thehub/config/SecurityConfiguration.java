946b8aa257668256ea292aa14eac56adfdf0571c
jsavell <contact@shadowater.com>
2016-03-15 20:01:14 -0500

    open up home page and posts api endpoint

--- src/main/java/com/shadowaterservices/thehub/config/SecurityConfiguration.java
+++ src/main/java/com/shadowaterservices/thehub/config/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/built/**", "/main.css").permitAll()
+				.antMatchers("/built/**", "/css/app.css","/api/posts","/").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
